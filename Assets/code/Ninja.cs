47efa8f2b071a37c2f701f7824ab2704bde6131b
Nguyen Dinh Quynh <nguyendinhquynh@Nguyens-MacBook-Pro.local>
2018-10-24 00:28:45 +0700

    upload

---
+++ Assets/code/Ninja.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ninja : MonoBehaviour
+{
+    private bool OnAir;
+    private Rigidbody2D ninja;
+    private float jump;
+    private float move = 30f;
+    // Use this for initialization
+    void Start()
+    {
+        ninja = GetComponent<Rigidbody2D>();
+        OnAir = false;
+        jump = 600f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            ninja.AddForce(new Vector2(move, 1.13f));
+            gameObject.transform.localScale = new Vector3(
+                Mathf.Abs(gameObject.transform.localScale.x),
+                gameObject.transform.localScale.y,
+                gameObject.transform.localScale.z);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            ninja.AddForce(new Vector2(-move, 1.13f));
+            gameObject.transform.localScale = new Vector3(
+                -Mathf.Abs(gameObject.transform.localScale.x),
+                gameObject.transform.localScale.y,
+                gameObject.transform.localScale.z);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (!OnAir)
+            {
+                ninja.velocity = Vector2.zero;
+                ninja.AddForce(new Vector2(1.47f, jump));
+                OnAir = true;
+            }
+        }
+    }
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+        OnAir = false;
+	}
+}
